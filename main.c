f71b9acb6d735ba4263f38a0722af5c7723fc5c3
MC-Kangan <97443307+MC-Kangan@users.noreply.github.com>
2022-02-01 16:10:03 +0000

    Exercise 2 Task 1 Modified (Complete the interrupt initialisation function and ISR to toggle the LED on RH3 when the LDR goes from light to dark. )

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #pragma config WDTE = OFF        // WDT operating mode (WDT enabled regardless of sleep)
 
 #include <xc.h>
-#include "LEDarray.h"
+//#include "LEDarray.h"
 #include "interrupts.h"
 #include "comparator.h"
 
@@ -16,6 +16,13 @@
 void main(void) {
 	//call your initialisation functions to set up the hardware modules
 
+     // setup pin for output (connected to LED2 - RH3)
+    LATHbits.LATH3=0;   //set initial output state - LATx registers (output latch)
+    TRISHbits.TRISH3=0; //set TRIS value for pin (output) - TRISx registers (data direction)
+    
+    Interrupts_init(); // Interrupt
+    Comp1_init();// Comparator
+    
     while (1) {
 		
     }
